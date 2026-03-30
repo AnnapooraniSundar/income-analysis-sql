0a5e337670fcd5088560670c5d76af92edf234de
AnnapooraniSundar <80017899+AnnapooraniSundar@users.noreply.github.com>
2026-03-30 11:21:47 +1100

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,56 +1,56 @@
 select count(*)
 from census;
-
+----------------------------
 SELECT income, count(*) as count
 FROM census
 group by income;
-
+-------------------------------
 select income, avg(age) as avg_age
 from census
 group by income;
-
+-----------------------------------
 select "education-num", income, count(*) as count
 from census
 group by "education-num", income
 order by "education-num";
 
-
+-------------------------------------
 SELECT "education_level", income, count(*) as count
 from census
 WHERE income = '>50K'
 group by "education_level", income
 order by count DESC;
-
+-------------------------------------
 SELECT "education_level" 
 from census;
-
+---------------------------------------
 SELECT 
     education_level,
     SUM(CASE WHEN income = '>50K' THEN 1 ELSE 0 END) * 1.0 / COUNT(*) AS high_income_ratio
 FROM census
 GROUP BY education_level
 ORDER BY high_income_ratio DESC;
-
+-----------------------------------------
 select income, avg(age) as avg_age
 from census
 group by income;
-
+-----------------------------------------------
 select education_level
 where income = '>50K' * 1.0 / count(*) as high_income_ratio
 from census
-
+--------------------------------------------------------------
 SELECT "education_level", COUNT(*) AS total
 FROM census
 GROUP BY "education_level"
 ORDER BY total DESC;
-
+---------------------------------------------------------------
 SELECT 
     "education_level",
     SUM(CASE WHEN income = '>50K' THEN 1 ELSE 0 END) * 1.0 / COUNT(*) AS high_income_ratio
 FROM census
 GROUP BY "education_level"
 ORDER BY high_income_ratio DESC;
-
+------------------------------------------------------------------
 select education_level, count(*) as total
 from census
 group by education_level
